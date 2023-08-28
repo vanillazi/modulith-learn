cd15d24998703629942d11594e50e0dffa55a6db
Vanillazi <vanillazi@wanazi.cn>
2023-08-28 09:08:19 +0800

    add actuator

--- src/main/java/cn/vanillazi/learn/article/ArticleController.java
+++ src/main/java/cn/vanillazi/learn/article/ArticleController.java
@@ -1,6 +1,6 @@
 package cn.vanillazi.learn.article;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +11,10 @@ import java.time.LocalDateTime;
 
 @RequestMapping("/article")
 @RestController
+@RequiredArgsConstructor
 public class ArticleController {
 
 	private final UserRepository userRepository;
-
-	@Autowired
-	public ArticleController(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
 
 	@GetMapping
 	public String getArticles() {
